328030550f453c4a2dbd31ebbf0cd191048a62bb
atinawldnnr <atinawldnnr@gmail.com>
2026-05-11 05:32:10 +0700

    improve ui

--- app.py
+++ app.py
@@ -7,12 +7,64 @@ env = os.getenv("APP_ENV", "development")
 
 @app.route('/')
 def home():
-    return jsonify({
-    "pesan": "Aplikasi Kehadiran Mahasiswa",
-    "status": "aktif",
-    "versi": "1.0.0",
-    "environment": env
-    })
+    return f"""
+    <html>
+    <head>
+        <title>Aplikasi Kehadiran Mahasiswa</title>
+        <style>
+            body {{
+                font-family: Arial;
+                background-color: #f4f4f4;
+                text-align: center;
+                padding-top: 100px;
+            }}
+
+            .card {{
+                background: white;
+                width: 500px;
+                margin: auto;
+                padding: 30px;
+                border-radius: 12px;
+                box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+            }}
+
+            h1 {{
+                color: #2c3e50;
+            }}
+
+            p {{
+                font-size: 18px;
+            }}
+
+            .status {{
+                color: green;
+                font-weight: bold;
+            }}
+        </style>
+    </head>
+
+    <body>
+        <div class="card">
+            <h1>Aplikasi Kehadiran Mahasiswa</h1>
+
+            <p>Status:
+                <span class="status">Aktif</span>
+            </p>
+
+            <p>Versi: 1.0.0</p>
+
+            <p>Environment: {env}</p>
+
+            <hr>
+
+            <h3>Endpoint API</h3>
+
+            <p>/health</p>
+            <p>/mahasiswa</p>
+        </div>
+    </body>
+    </html>
+    """
 
 @app.route('/health')
 def health():
